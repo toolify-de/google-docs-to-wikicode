f1b0578fd333c1a489840c071c7ec70316f40bf1
Eleyine Zarour <eleyine@gmail.com>
2011-05-01 17:28:22 -0400

    temp save

--- wikicodify.py
+++ wikicodify.py
@@ -34,7 +34,47 @@ def getFormat(attribute, wikidict, tag):
 				 format = wikidict[attribute][class_name]
 	return format
 
+def printFormat(tagType, attribute, format_stack, special_stack, output):
+	# if the tag is an opening tag
+	if tagType == 'opening':
+		start_format = format_stack[-1][0] # i.e. last start_format	
+		if attribute == 'li':
+			# special_stack['li'] is a list of tuples ('degree', 'format')
+			if len(special_stack['li']) == 0:
+				bullets = '\n*'
+			else:
+				lastli = special_stack['li'][-1]
+				degree = lastli[0]
+				bullets = lastli[1]
+				if (degree - start_format) > 1: 
+					# if the level difference is more than 1, then most likely reset the list
+					# I'm aware this is a flaw, but I'll keep it like this until I find a better solution
+					bullets = '\n*'
+				elif start_format > degree:
+					bullets = bullets + '*'
+				elif start_format < degree:
+					bullets = bullets[0:-1]
+				else:
+					bullets = bullets
+					# It means start_format == bullets, in other words, don't change the list level.
+			special_stack['li'].append((start_format, bullets))
+			special_stack['format_len'].append(len(bullets))
+			output.write(bullets)
+		else:
+			special_stack['format_len'].append(len(start_format))
+			output.write(start_format)
+	# if the tag is a closing tag
+	if tagType == 'closing':
+		format = format_stack.pop()
+ 		if attribute != 'li' and attribute != 'ol':
+			end_format = format[1]
+			output.write(end_format)		
+	return {'format_stack': format_stack, 'special_stack': special_stack}
+
+def erasePreviousFormat(special_stack, output):
+	length = format_stack[-1][0]
 def wikicodify(input_filename, output_filename, wikidict):
+	print wikidict
 	
 	# Get Google Docs html
 	input_file = open(input_filename, 'r')
@@ -47,8 +87,10 @@ def wikicodify(input_filename, output_filename, wikidict):
 	# Define tags
 	tagRegex = re.compile('<.*?>')
 	iterator = tagRegex.finditer(html)
-	attribute_stack = []
 	format_stack = []
+	
+	# Special elements
+	special_stack = {'li': []}
 	
 	content_start = 0
 	for match in iterator:
@@ -57,16 +99,20 @@ def wikicodify(input_filename, output_filename, wikidict):
 		if(tagType(match.group()) == 'opening'):
 			format = getFormat(attribute, wikidict, match.group())
 			format_stack.append(format)
-			output.write(format[0])
+			stacks = printFormat('opening', attribute, format_stack, special_stack, output)
+			format_stack = stacks['format_stack']
+			special_stack = stacks['special_stack']
 			content_start = match.end()
 		else:
 			content_end = match.start()
 			# Print all the contents except the css
 			if (attribute != 'style'):
-				output.write(html[content_start:content_end])
-			format = format_stack.pop()
-			output.write(format[1])
+					output.write(html[content_start:content_end])
+			stacks = printFormat('closing', attribute, format_stack, special_stack, output)
+			format_stack = stacks['format_stack']
+			special_stack = stacks['special_stack']			
 			content_start = match.end()
+			
 	output.close()
 	print 'Html to wikicode success!'
 
@@ -74,6 +120,6 @@ def wikicodify(input_filename, output_filename, wikidict):
 # input_file = raw_input('Enter .html file: ')
 # output_file = raw_input('Enter output file: ')
 # Get Google Docs css dictionary
-wikidict_object = wikidict.Wikidict('SimpleDoc.html')
+wikidict_object = wikidict.Wikidict('BIOL201LN21.html')
 wikidict = wikidict_object.dict
-wikicodify('SimpleDoc.html', 'test1.txt', wikidict)
+wikicodify('BIOL201LN21.html', 'test1.txt', wikidict)
