d9d0b660e6809d6d8cfd25ca878b1565382e2fe9
Eleyine Zarour <eleyine@gmail.com>
2011-05-01 19:22:29 -0400

    temp save, trying to fix decimal lists vs bullet lists bug

--- wikicodify.py
+++ wikicodify.py
@@ -12,7 +12,7 @@ def getAttribute(tag):
 
 # Uses two regex'es, can't find anything better for now
 def getClass(tag):
-  class_regex = re.compile('class="(.*)"')
+  class_regex = re.compile('class="(.*?)"')
   class_content = class_regex.search(tag).group(1)
   attr_regex = re.compile('[^ ]+')
   attr_match = attr_regex.findall(class_content)
@@ -29,9 +29,10 @@ def getFormat(attribute, wikidict, tag):
 			format = wikidict[attribute]['noclass']
 		else:
 			classes = getClass(tag)
+			print 'classes are', classes
 			for class_name in classes:
 				if class_name in wikidict[attribute].keys():
-				 format = wikidict[attribute][class_name]
+					format = wikidict[attribute][class_name]
 	return format
 
 def printFormat(tagType, attribute, format_stack, special_stack, output):
@@ -40,33 +41,37 @@ def printFormat(tagType, attribute, format_stack, special_stack, output):
 		start_format = format_stack[-1][0] # i.e. last start_format	
 		if attribute == 'li':
 			# special_stack['li'] is a list of tuples ('degree', 'format')
+			bullet = special_stack['litype']
+			print 'bullet is', bullet
 			if len(special_stack['li']) == 0:
-				bullets = '\n*'
+				bullets = '\n' + bullet
 			else:
 				lastli = special_stack['li'][-1]
 				degree = lastli[0]
 				bullets = lastli[1]
 				if (degree - start_format) > 1: 
-					# if the level difference is more than 1, then most likely reset the list
+					# if the level difference is more than 1, then reset the list
 					# I'm aware this is a flaw, but I'll keep it like this until I find a better solution
-					bullets = '\n*'
+					bullets = '\n' + bullet
 				elif start_format > degree:
-					bullets = bullets + '*'
+					bullets = bullets + bullet
 				elif start_format < degree:
 					bullets = bullets[0:-1]
 				else:
 					bullets = bullets
 					# It means start_format == bullets, in other words, don't change the list level.
 			special_stack['li'].append((start_format, bullets))
-			special_stack['format_len'].append(len(bullets))
 			output.write(bullets)
+		elif attribute == 'ol':
+			print 'ol is of type', format_stack[-1]
+			special_stack['litype'] = format_stack[-1]
 		else:
-			special_stack['format_len'].append(len(start_format))
 			output.write(start_format)
 	# if the tag is a closing tag
 	if tagType == 'closing':
 		format = format_stack.pop()
- 		if attribute != 'li' and attribute != 'ol':
+  		if attribute != 'li' and attribute != 'ol' and attribute != 'td':
+			print 'attribute is', attribute, 'and format is', format
 			end_format = format[1]
 			output.write(end_format)		
 	return {'format_stack': format_stack, 'special_stack': special_stack}
@@ -90,7 +95,7 @@ def wikicodify(input_filename, output_filename, wikidict):
 	format_stack = []
 	
 	# Special elements
-	special_stack = {'li': []}
+	special_stack = {'li': [], 'litype': '*'}
 	
 	content_start = 0
 	for match in iterator:
