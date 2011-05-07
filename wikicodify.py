0197f14bd7195f0da778ef291994b3b7644d6676
Eleyine Zarour <eleyine@gmail.com>
2011-05-07 00:10:53 -0400

    tables can now be converted

--- wikicodify.py
+++ wikicodify.py
@@ -17,6 +17,10 @@ def get_class(tag):
   class_content = class_regex.search(tag).group(1)
   return class_content.split()
 
+# assumes opening tag
+def get_attributes(tag):
+	return re.compile('(\w*)="([^"]*)"').findall(tag)
+
 def get_tag_type(tag):
   closing_tag = re.compile('^</')
   return 'opening' if (closing_tag.match(tag) is None) else 'closing'
@@ -45,13 +49,18 @@ def get_format(element, wikidict, tag):
 				if class_name in element_classes:
 					formats.append(wikidict[element][class_name])
 		if element == 'table':
+			for attribute in get_attributes(tag):
+				if attribute[0] == 'class':
+					1 #formats.append(wikidict[element][class_name])
+				else:
+					formats.append([' ' + attribute[0] + '="' + attribute[1] + '"', ''])
 			1 # TODO: take into account table attributes (cellpadding, cellspacing, border etc.)
 	format_before = ''
 	format_after = ''
 	for format_list in formats:
 			if len(format_list) == 2:
 				format_before += format_list[0]
-				format_after += format_list[1]
+				format_after = format_list[1] + format_after
 			else:
 				return format_list[0]
 	return [format_before, format_after]
@@ -79,6 +88,8 @@ def print_format(tag_type, element, elements_stack, output):
 	if tag_type == 'opening':
 		if element == 'p':
 			print elements_stack['elements']
+		if element == 'table':
+			elements_stack['table']['insert_break'] = False
 		if element == 'li':
 			elements_stack = update_elements_stack(elements_stack)
 			bullet_type = elements_stack['li']['bullet_type']
@@ -88,7 +99,10 @@ def print_format(tag_type, element, elements_stack, output):
 		elif element == 'ol':
 			elements_stack['li']['bullet_type'] = elements_stack['formats'][-1]
 		elif element == 'p'and elements_stack['elements'][-2] == 'td':
-			1 # write nothing (p defaults to a return carriage and we don't want that)
+				if elements_stack['table']['insert_break'] == False:
+					elements_stack['table']['insert_break'] = True
+				else:
+					output.write('<br/>') # write nothing (p defaults to a return carriage and we don't want that)
 		else:
 			format = elements_stack['formats'][-1][0]
 			if len(format) > 0:
@@ -130,7 +144,10 @@ def wikicodify(input_filename, output_filename, wikidict):
 		for j in i:
 			liref.append(j)
 	liref.sort()
-	elements_stack = {'li': {'li_bullet_type' : '*','limap' :{}, 'liref' : liref }, 'formats': [], 'elements' : [] }
+	elements_stack = {'li': {'li_bullet_type' : '*','limap' :{}, 'liref' : liref }, 
+					  'table': {'insert_break': False},
+					  'formats': [], 
+					  'elements' : [] }
 	
 	content_start = 0
 	ignore_br_at = 0
@@ -166,7 +183,7 @@ def wikicodify(input_filename, output_filename, wikidict):
 # input_file = raw_input('Enter .html file: ')
 # output_file = raw_input('Enter output file: ')
 # Get Google Docs css dictionary
-input_file = 'SimpleTable.html'
+input_file = 'BIOL201LN21.html'
 output_file = 'test.txt'
 wikidict_object = wikidict.Wikidict(input_file)
 wikidict = wikidict_object.dict
